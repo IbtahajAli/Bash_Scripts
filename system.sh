bafedce596064ccbb96c0271e5a4d6fde382fbf5
ibtahaj <ibtahajgrt@gmail.com>
2024-06-26 21:32:32 +0500

    Change in scripts

--- system.sh
+++ system.sh
@@ -2,10 +2,10 @@
 
 Name=$(whoami)
 CPU=$(nproc)
-RAM=$(grep -i 'memtotal' /proc/meminfo | awk '{print $2 / 1024 / 1024 " GB"}')
+RAM=$(grep -i 'memtotal' /proc/meminfo | awk '{print $2 /(1024^2)}')
 HDD=$(lsblk -b -d -o NAME,SIZE | grep -E 'sda|sdb' | awk '{sum += $2} END {print sum / (1024^3)}')
 
-echo "Name : $Name "
-echo "Cpu : $CPU CPU "
-echo "RAM : $RAM "
+echo "Name: $Name "
+echo "CPU : $CPU CPU "
+echo "RAM : $RAM GB "
 echo "HDD : $HDD GB "
